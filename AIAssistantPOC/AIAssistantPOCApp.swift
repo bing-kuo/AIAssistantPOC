19a6591daa605f57172133e090244292d52de91d
bingkuo <bingyukuo@gmail.com>
2026-06-05 10:28:56 +0800

    Initial Commit

---
+++ AIAssistantPOC/AIAssistantPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIAssistantPOCApp.swift
+//  AIAssistantPOC
+//
+//  Created by Bing on 2026/6/5.
+//
+
+import SwiftUI
+
+@main
+struct AIAssistantPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
